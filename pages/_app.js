a9e60530d70cd5f803b3a8465f4e0ec2359abdfc
mhmaarafath <mhmaarafath@gmail.com>
2023-04-01 13:35:07 +0530

    antd tailwind installation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { StyleProvider } from '@ant-design/cssinjs';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyleProvider hashPriority='high'>
+      <Component {...pageProps} />
+    </StyleProvider>
+  )
 }
