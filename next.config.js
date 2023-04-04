18ea63eaaa05afe3524fba5eb5361718eba6dd6e
mhmaarafath <mhmaarafath@gmail.com>
2023-04-04 15:20:45 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['erabiz-api.test'],
+    domains: ['erabiz-api.test', 'ultimomart.com'],
   },
   reactStrictMode: true,
 }
